c19fbb80b1076cd6a0fb6b4a184d11ee7bd06c23
loki411 <antzlvssteph@gmail.com>
2013-05-05 13:27:58 +0100

    updated spree installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beapp::Application.config.secret_token = '30261f9bec92ed9a8abd62b97d53052480bda08ae35125d1045c3ed7ca996fbb9f7e44d25dce633d254162eed2c16cedf0b00432182a39bd4785451d006b6d96'
